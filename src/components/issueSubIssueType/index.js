a77b52aec7a2cef942dfe75ddb6238ec56267081
Elonmusk <srujanaj313@gmail.com>
2024-08-15 16:47:17 +0530

    common component changes

---
+++ src/components/issueSubIssueType/index.js
@@ -0,0 +1,195 @@
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Stack,
+  Select,
+  MenuItem,
+  FormControl,
+  FormControlLabel,
+  Checkbox,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { v4 as uuidv4 } from "uuid";
+import {
+  reschedulingIssueTypesListURL,
+  reschedulingSubIssueListURL,
+} from "../../helpers/Urls";
+import client from "../../helpers/Api";
+
+const IssueSubIssueType = ({ formik }) => {
+  const [issueTypes, setIssueTypes] = useState([]);
+  const [subIssueTypes, setSubIssueTypes] = useState([]);
+  useEffect(() => {
+    const fetchIssueTypes = async () => {
+      try{
+        const data =
+          process.env.REACT_APP_ENV === "mockserver"
+            ? await client.get(reschedulingIssueTypesListURL)
+            : await client.get(reschedulingIssueTypesListURL);
+        setIssueTypes(data);
+      }catch(err){
+        console.error('Error in fetchIssueTypes', err);
+      }
+    };
+    fetchIssueTypes();
+  }, []);
+
+  const fetchSubIssueTypes = async (issueId) => {
+    try{
+      const data =
+        process.env.REACT_APP_ENV === "mockserver"
+          ? await client.get(reschedulingSubIssueListURL)
+          : await client.get(`${reschedulingSubIssueListURL}/${issueId}`);
+      setSubIssueTypes(data);
+    }catch(err){
+      console.error('Error in fetchSubIssueTypes', err);
+    }
+  };
+
+  const addIssue = () => {
+    formik.setFieldValue("issues", [
+      ...formik.values.issues,
+      {
+        id: uuidv4(),
+        issueChecked: false,
+        issueType: "",
+        subIssueType: "",
+        issueStartDate: null,
+      },
+    ]);
+  };
+
+  const handleFieldChange = (id, field, value) => {
+    const updatedIssues = formik.values.issues.map((issue) =>
+      issue.id === id ? { ...issue, [field]: value } : issue
+    );
+    formik.setFieldValue("issues", updatedIssues);
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <form onSubmit={formik.handleSubmit}>
+        <Stack spacing={2}>
+          {formik.values.issues.map((element) => (
+            <Stack
+              key={element.id}
+              direction="row"
+              spacing={2}
+              alignItems="center"
+            >
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={element.issueChecked}
+                    onChange={(e) =>
+                      handleFieldChange(
+                        element.id,
+                        "issueChecked",
+                        e.target.checked
+                      )
+                    }
+                    name="issueChecked"
+                    sx={{ py: 0 }}
+                  />
+                }
+                label={"Issue Type:"}
+              />
+              <FormControl
+                variant="outlined"
+                sx={{ minWidth: 200 }}
+                size="small"
+                error={Boolean(
+                  formik.errors.issues?.find((issue) => issue.id === element.id)
+                    ?.issueType
+                )}
+              >
+                <Select
+                  label="Issue Type"
+                  value={element.issueType.nmiDesc}
+                  onChange={(e) => {
+                    const issueType = issueTypes.find(i => i.nmiDesc === e.target.value);
+                    handleFieldChange(element.id, "issueType", issueType);
+                    fetchSubIssueTypes(issueType.nmiId);
+                  }}
+                  required
+                >
+                  {issueTypes?.map((issue) => (
+                    <MenuItem key={issue.nmiId} value={issue.nmiDesc}>
+                      {issue.nmiDesc}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+
+              <Typography>- Issue Sub:</Typography>
+
+              <FormControl
+                variant="outlined"
+                sx={{ minWidth: 200 }}
+                size="small"
+                error={Boolean(
+                  formik.errors.issues?.find((issue) => issue.id === element.id)
+                    ?.subIssueType
+                )}
+              >
+                <Select
+                  value={element.subIssueType.nmiDesc}
+                  onChange={(e) =>{
+                    const subIssueType = subIssueTypes.find(s => s.nmiDesc === e.target.value);
+                    handleFieldChange(
+                      element.id,
+                      "subIssueType",
+                      subIssueType
+                    )}
+                  }
+                >
+                  {subIssueTypes?.map((subIssue) => (
+                    <MenuItem key={subIssue.nmiId} value={subIssue.nmiDesc}>
+                      {subIssue.nmiDesc}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+
+              <DatePicker
+                label="*Start Date"
+                value={element.issueStartDate}
+                onChange={(date) => {
+                  handleFieldChange(element.id, "issueStartDate", date);
+                }}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    size="small"
+                    variant="outlined"
+                    error={Boolean(
+                      formik.errors.issues?.find(
+                        (issue) => issue.id === element.id
+                      )?.issueStartDate
+                    )}
+                    helperText={
+                      formik.errors.issues?.find(
+                        (issue) => issue.id === element.id
+                      )?.issueStartDate
+                    }
+                  />
+                )}
+              />
+            </Stack>
+          ))}
+          <Stack direction="row" justifyContent="flex-end" spacing={2}>
+            <Button variant="text" type="Button" onClick={addIssue}>
+              + Add more
+            </Button>
+          </Stack>
+        </Stack>
+      </form>
+    </LocalizationProvider>
+  );
+};
+
+export default IssueSubIssueType;
--- src/pages/dashboard/interviewCalendarView/RescheduleRequest.js
+++ src/pages/dashboard/interviewCalendarView/RescheduleRequest.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import {
   TextField,
@@ -7,38 +7,39 @@ import {
   MenuItem,
   FormControl,
   InputLabel,
+  Typography,
   FormControlLabel,
-  Checkbox,
+  RadioGroup,
+  Radio,
 } from "@mui/material";
 import { reschedulingReasonsListURL } from "../../../helpers/Urls";
 import client from "../../../helpers/Api";
-import Stack from "@mui/material/Stack";
-
-import Typography from "@mui/material/Typography";
 import { STATES } from "../../../helpers/Constants";
+import IssueSubIssueType from "../../../components/issueSubIssueType";
+import { useFormik } from "formik";
+// import * as Yup from "yup";
+import { v4 as uuidv4 } from "uuid";
 
 function RescheduleRequest({ onCancel }) {
+  const formik = useFormik({
+    initialValues: {
+      issues: [
+        {
+          id: uuidv4(),
+          issueChecked: false,
+          issueType: "",
+          subIssueType: "",
+          issueStartDate: null,
+        },
+      ],
+    },
+    onSubmit: (values) => {
+      console.log("Form Values", values);
+      onCancel();
+    },
+  });
+
   const states = STATES;
-
-  const issueTypes = [
-    {
-      nmiId: 1,
-      nmiDesc: "Ability",
-    },
-    {
-      nmiId: 2,
-      nmiDesc: "Actively Seeking Work",
-    },
-    {
-      nmiId: 49,
-      nmiDesc: "Attending Training",
-    },
-    {
-      nmiId: 55,
-      nmiDesc: "Expected return to work",
-    },
-  ];
-
   const [rescheduleTo, setRescheduleTo] = useState("");
   const [reasonForRescheduling, setReasonForRescheduling] = useState("");
   const [additionalDetails, setAdditionalDetails] = useState("");
@@ -56,201 +57,160 @@ function RescheduleRequest({ onCancel }) {
     fetchRescheduleReasonsListData();
   }, []);
 
-  const handleSubmit = () => {
-    onCancel();
-  };
-
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          label="Reschedule to"
-          size="small"
-          value={rescheduleTo}
-          onChange={(e) => setRescheduleTo(e.target.value)}
-          variant="outlined"
-          fullWidth
-          required
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <FormControl size="small" fullWidth>
-          <InputLabel id="reschedule-request-dropdown">
-            Reason for rescheduling *
-          </InputLabel>
-          <Select
-            label="Reason for rescheduling"
-            value={reasonForRescheduling}
-            onChange={(e) => setReasonForRescheduling(e.target.value)}
-            required
-            labelId="reschedule-request-dropdown"
-          >
-            {reasons.map((reason) => (
-              <MenuItem key={reason.id} value={reason.id}>
-                {reason.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </Grid>
-      {reasonForRescheduling === 3333 ? (
-        <>
-          <Grid item xs={12} sm={12}>
-            <Typography className="label-text" marginTop={"8px !important"}>
-              Please provide below details:
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label="Date*"
-              size="small"
-              value={additionalDetails}
-              onChange={(e) => setAdditionalDetails(e.target.value)}
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label="Time*"
-              size="small"
-              value={staffNotes}
-              onChange={(e) => setStaffNotes(e.target.value)}
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label="City*"
-              size="small"
-              value={staffNotes}
-              onChange={(e) => setStaffNotes(e.target.value)}
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-          <FormControl size="small">
-          <InputLabel id="state-dropdown">State*</InputLabel>
-            <Select
-              // {...field}
-              label="State*"
-              variant="outlined"
-              labelId="state-dropdown"
-              sx={{ width: "160px", height:"35px" }}
-            >
-              {states.map((option) => (
-                <MenuItem key={option.id} value={option.id}>
-                  {option.id}
-                </MenuItem>
-              ))}
-            </Select>
-            </FormControl>
-          </Grid>
-        </>
-      ) : (
-        <></>
-      )}
-
-      <Grid item xs={12} sm={6}>
-        <TextField
-          label="Additional details, if any"
-          size="small"
-          value={additionalDetails}
-          onChange={(e) => setAdditionalDetails(e.target.value)}
-          variant="outlined"
-          multiline
-          rows={3}
-          fullWidth
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          label="Staff Notes, if any"
-          size="small"
-          value={staffNotes}
-          onChange={(e) => setStaffNotes(e.target.value)}
-          variant="outlined"
-          multiline
-          rows={3}
-          fullWidth
-        />
-      </Grid>
-      <Typography className="label-text" marginTop={"8px !important"}>
-        Create issues, if any, based on the information associated with this
-        request:
-      </Typography>
+    <form onSubmit={formik.handleSubmit}>
       <Stack spacing={2}>
-        {/* {[1,2,3].map((issue, index) => ( */}
-        <Stack direction="row" spacing={2} alignItems="center">
-          <FormControlLabel
-            control={
-              <Checkbox
-                // checked={!!issue.createdIssue}
-                onChange={
-                  (e) => []
-                  // handleIssueChange(index, "createdIssue", e.target.value)
-                }
-                name="createdIssue"
-                sx={{ py: 0 }}
-              />
-            }
-            label={"issueType:"}
-          />
-          <FormControl variant="outlined" sx={{ minWidth: 200 }} size="small">
+        <Stack direction={"column"} spacing={2}>
+          <Stack direction={"row"} justifyContent={"space-between"}>
+            <TextField
+              label="Reschedule to"
+              size="small"
+              value={rescheduleTo}
+              onChange={(e) => setRescheduleTo(e.target.value)}
+              variant="outlined"
+              required
+              sx={{ width: "40%" }}
+            />
+            <FormControl sx={{width:"30%",display:'flex', flexDirection:'row'}}>
+              <Typography
+                sx={{
+                  width: "20%",
+                  alignSelf: "center",
+                }}
+              >
+                *Mode:
+              </Typography>
+              <RadioGroup
+                row
+                name="mode"
+                value={formik.values.workMode}
+                onChange={formik.handleChange}
+              >
+                <FormControlLabel
+                  value="1"
+                  control={<Radio />}
+                  label="in person"
+                />
+                <FormControlLabel
+                  value="2"
+                  control={<Radio />}
+                  label="virtual"
+                />
+              </RadioGroup>
+            </FormControl>
+          </Stack>
+          <FormControl size="small" fullWidth>
+            <InputLabel id="reschedule-request-dropdown">
+              Reason for rescheduling *
+            </InputLabel>
             <Select
               label="Reason for rescheduling"
               value={reasonForRescheduling}
               onChange={(e) => setReasonForRescheduling(e.target.value)}
               required
               labelId="reschedule-request-dropdown"
+              sx={{ width: "50%" }}
             >
-              {issueTypes.map((issue) => (
-                <MenuItem key={issue.nmiId} value={issue.nmiDesc}>
-                  {issue.nmiDesc}
+              {reasons.map((reason) => (
+                <MenuItem key={reason.id} value={reason.id}>
+                  {reason.name}
                 </MenuItem>
               ))}
             </Select>
           </FormControl>
-          <Typography>- Issue Sub:</Typography>
-          <FormControl variant="outlined" sx={{ minWidth: 200 }} size="small">
-            {/* <InputLabel>Issue Sub</InputLabel> */}
-            <Select
-              // value={issue.issueSub}
-              onChange={
-                (e) => []
-                // handleIssueChange(index, "issueSub", e.target.value)
-              }
-              // label="Issue Sub"
-            >
-              <MenuItem key={"sub"} value={"Sub 1"}>
-                Sub 1
-              </MenuItem>
-            </Select>
-          </FormControl>
         </Stack>
-        {/* ))} */}
-        <Stack
-          direction="row"
-          justifyContent={"center"}
-          sx={{ mt: "0px !important" }}
-        >
-          <Button variant="text" onClick={() => {}}>
-            + Add more
-          </Button>
+
+        {reasonForRescheduling === 3159 || reasonForRescheduling === 3160 ? (
+          <>
+            <Typography className="label-text" marginTop={"8px !important"}>
+              Please provide below details:
+            </Typography>
+            <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+              <TextField
+                label="Date*"
+                size="small"
+                value={additionalDetails}
+                onChange={(e) => setAdditionalDetails(e.target.value)}
+                variant="outlined"
+                fullWidth
+              />
+              <TextField
+                label="Time*"
+                size="small"
+                value={staffNotes}
+                onChange={(e) => setStaffNotes(e.target.value)}
+                variant="outlined"
+                fullWidth
+              />
+            </Stack>
+            <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+              <TextField
+                label="City*"
+                size="small"
+                value={staffNotes}
+                onChange={(e) => setStaffNotes(e.target.value)}
+                variant="outlined"
+                fullWidth
+              />
+              <FormControl size="small" fullWidth>
+                <InputLabel id="state-dropdown">State*</InputLabel>
+                <Select
+                  label="State*"
+                  variant="outlined"
+                  labelId="state-dropdown"
+                >
+                  {states.map((option) => (
+                    <MenuItem key={option.id} value={option.id}>
+                      {option.id}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Stack>
+          </>
+        ) : null}
+
+        <Stack direction={"column"} spacing={2}>
+          <TextField
+            label="Additional details, if any"
+            size="small"
+            value={additionalDetails}
+            onChange={(e) => setAdditionalDetails(e.target.value)}
+            variant="outlined"
+            multiline
+            rows={3}
+            fullWidth
+          />
+          <TextField
+            label="Staff Notes, if any"
+            size="small"
+            value={staffNotes}
+            onChange={(e) => setStaffNotes(e.target.value)}
+            variant="outlined"
+            multiline
+            rows={3}
+            fullWidth
+          />
         </Stack>
-      </Stack>
-      <Grid item xs={12} container justifyContent="flex-end" spacing={2}>
-        <Grid item>
-          <Button variant="contained" onClick={handleSubmit}>
+
+        <Typography className="label-text" marginTop={"8px !important"}>
+          Create issues, if any, based on the information associated with this
+          request:
+        </Typography>
+        <Stack spacing={2}>
+          <IssueSubIssueType formik={formik} />
+        </Stack>
+
+        <Stack direction="row" spacing={2} justifyContent="flex-end">
+          <Button variant="contained" color="primary" type="submit">
             Submit
           </Button>
-        </Grid>
-        <Grid item>
           <Button variant="outlined" onClick={onCancel}>
             Cancel
           </Button>
-        </Grid>
-      </Grid>
-    </Grid>
+        </Stack>
+      </Stack>
+    </form>
   );
 }
 
